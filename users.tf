029ae531a446b853d29701cf44ce2acaee4dd655
Edwin de Jong <edejong@fastmail.fm>
2021-03-28 20:23:31 +0200

    Specific policy for user github_actions_recipes.

--- users.tf
+++ users.tf
@@ -3,36 +3,25 @@ resource "aws_iam_user" "recipes" {
   path = "/system/"
 }
 
-resource "aws_iam_access_key" "lb" {
-  user = aws_iam_user.recipes.name
-}
-
-/*
 data "aws_iam_policy_document" "recipes_pd" {
   statement {
-    actions = [ ]
+    actions = [
+      "s3:DeleteObject",
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:PutObject"
+    ]
     effect = "Allow"
-    resources = [module.aws_static_website]
+    resources = [
+      "arn:aws:s3:::${module.aws_static_website.website_root_s3_bucket}",
+      "arn:aws:s3:::${module.aws_static_website.website_root_s3_bucket}/*",
+    ]
   }
 }
 
 resource "aws_iam_user_policy" "lb_ro" {
-  name = "test"
-  user = aws_iam_user.lb.name
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "ec2:Describe*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
+  name   = "test"
+  user   = aws_iam_user.recipes.name
+  policy = data.aws_iam_policy_document.recipes_pd.json
 }
-EOF
-}
-*/
