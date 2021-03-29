65dca8f02803cd2b533bb63365c35ee226e08544
Edwin de Jong <edejong@fastmail.fm>
2021-03-29 11:13:13 +0200

    AWS Config plus logging

--- main.tf
+++ main.tf
@@ -23,6 +23,36 @@ provider "aws" {
   secret_key              = var.secret_key
 }
 
+locals {
+  config_logs_bucket_name = "aws_config_logs"
+}
+
+module "config_logs" {
+  source  = "trussworks/logs/aws"
+  version = "~> 10"
+
+  s3_bucket_name     = local.config_logs_bucket_name
+  allow_config       = true
+  config_logs_prefix = "config"
+  force_destroy      = true
+}
+
+module "config" {
+  source                                  = "trussworks/config/aws"
+  version                                 = "4.3.0"
+  config_logs_bucket                      = module.config_logs.aws_logs_bucket
+  config_logs_prefix                      = "config"
+  check_cloudtrail_enabled                = true
+  check_guard_duty                        = true
+  check_s3_bucket_public_write_prohibited = true
+  check_s3_bucket_ssl_requests_only       = true
+
+  tags = {
+    "Automation" = "Terraform"
+    "Name"       = "main_config"
+  }
+}
+
 data "aws_ami" "aws" {
   filter {
     name   = "name"
@@ -33,90 +63,90 @@ data "aws_ami" "aws" {
 }
 
 resource "aws_security_group" "default_security_group" {
-  name = "default-security-group"
+  name        = "default-security-group"
   description = "launch-wizard-1 created 2019-01-28T21:52:58.113+01:00"
 
   egress {
-    from_port = 0
-    protocol  = "-1"
-    to_port   = 0
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description = "HTTPS"
-    from_port = 443
-    protocol  = "tcp"
-    to_port   = 443
+    description      = "HTTPS"
+    from_port        = 443
+    protocol         = "tcp"
+    to_port          = 443
     ipv6_cidr_blocks = ["::/0"]
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   ingress {
-    description = "MQTTS"
-    from_port = 8883
-    to_port   = 8883
-    protocol  = "tcp"
+    description      = "MQTTS"
+    from_port        = 8883
+    to_port          = 8883
+    protocol         = "tcp"
     ipv6_cidr_blocks = ["::/0"]
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   ingress {
-    description = "Syncthing"
-    from_port = 22000
-    to_port   = 22000
-    protocol  = "tcp"
+    description      = "Syncthing"
+    from_port        = 22000
+    to_port          = 22000
+    protocol         = "tcp"
     ipv6_cidr_blocks = ["::/0"]
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   ingress {
-    description = "collectd udp"
-    from_port = 25826
-    to_port   = 25826
-    protocol  = "udp"
+    description      = "collectd udp"
+    from_port        = 25826
+    to_port          = 25826
+    protocol         = "udp"
     ipv6_cidr_blocks = ["::/0"]
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   ingress {
-    description = "collectd tcp"
-    from_port = 25826
-    to_port   = 25826
-    protocol  = "tcp"
+    description      = "collectd tcp"
+    from_port        = 25826
+    to_port          = 25826
+    protocol         = "tcp"
     ipv6_cidr_blocks = ["::/0"]
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   ingress {
     description = "SSH from my IP"
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["80.100.196.53/32"]
   }
 }
 
 resource "aws_instance" "influxdb" {
-  ami           = data.aws_ami.aws.id
-  instance_type = "t3.small"
-  availability_zone = "eu-central-1b"
-  ebs_optimized = true
+  ami                     = data.aws_ami.aws.id
+  instance_type           = "t3.small"
+  availability_zone       = "eu-central-1b"
+  ebs_optimized           = true
   disable_api_termination = false
-  cpu_core_count = 1
-  cpu_threads_per_core = 2
-  hibernation = false
-  key_name = "cloud-max-1"
-  monitoring = false
-  security_groups = [aws_security_group.default_security_group.name]
+  cpu_core_count          = 1
+  cpu_threads_per_core    = 2
+  hibernation             = false
+  key_name                = "cloud-max-1"
+  monitoring              = false
+  security_groups         = [aws_security_group.default_security_group.name]
 }
 
 resource "aws_ebs_volume" "influxdb_volume" {
   availability_zone = "eu-central-1b"
-  iops = 150
-  type = "gp2"
-  size = 50
-  snapshot_id = "snap-0ec16755d26518f7e"
+  iops              = 150
+  type              = "gp2"
+  size              = 50
+  snapshot_id       = "snap-0ec16755d26518f7e"
 }
 
 resource "aws_volume_attachment" "influxdb_volume_attachment" {
@@ -134,7 +164,7 @@ resource "aws_route53_record" "influx_to_instance" {
   type    = "CNAME"
   zone_id = aws_route53_zone.dejongsoftwareengineering_zone.id
   records = [aws_instance.influxdb.public_dns]
-  ttl = 300
+  ttl     = 300
 }
 
 resource "aws_route53_record" "grafana_to_instance" {
@@ -142,7 +172,7 @@ resource "aws_route53_record" "grafana_to_instance" {
   type    = "CNAME"
   zone_id = aws_route53_zone.dejongsoftwareengineering_zone.id
   records = [aws_instance.influxdb.public_dns]
-  ttl = 300
+  ttl     = 300
 }
 
 module "aws_static_website" {
