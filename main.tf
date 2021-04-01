f9cede15c0679d19c8069695256558580698188d
Edwin de Jong <edejong@fastmail.fm>
2021-04-01 19:45:42 +0200

    Correct provider configuration

--- main.tf
+++ main.tf
@@ -17,18 +17,16 @@ terraform {
 }
 
 provider "aws" {
-  region                  = "eu-central-1"
-  shared_credentials_file = "~/.aws/credentials"
-  access_key              = var.access_key
-  secret_key              = var.secret_key
+  region     = "eu-central-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 provider "aws" {
-  region                  = "us-east-1"
-  alias                   = "us-east-1"
-  shared_credentials_file = "~/.aws/credentials"
-  access_key              = var.access_key
-  secret_key              = var.secret_key
+  region     = "us-east-1"
+  alias      = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 locals {
